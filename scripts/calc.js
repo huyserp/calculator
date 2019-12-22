ad2f68235c523926e3bd255c089efe0b3bb083cb
Peter Huyser <huyserp@gmail.com>
2019-12-22 17:57:35 +0100

    minimal progress on operator buttons....

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,5 @@
 const numDisplay = document.querySelector('.numdisplay');
+let numArr = [];
 
 //////////////////////////////////////////////////
 ///////////////// Number Buttons /////////////////
@@ -54,26 +55,21 @@ btnNine.addEventListener('click', () => {
     numDisplay.textContent += btnNine.textContent;});
 
 
-////////////////////////////////////////////////////    
-///////////////// Operator Buttons /////////////////
-////////////////////////////////////////////////////
-
-let oppArray = [];
-
-const btnDivide = document.querySelector('.divide');
-const btnMultiply = document.querySelector('.multiply');
-const btnSubtract = document.querySelector('.subtract');
-const btnAdd = document.querySelector('.add');
-const btnEquals = document.querySelector('.equals');
-
-
 ///////////////////////////////////////////////////
 ///////////////// Control Buttons /////////////////
 ///////////////////////////////////////////////////
 
+function clearDisplay() {
+    numDisplay.textContent = '';
+}
+function clearAll() {
+    numArr = [];
+}
+
 const btnClear = document.querySelector('.clear');
 btnClear.addEventListener('click', () => {
-    numDisplay.textContent = '';
+    clearDisplay();
+    clearAll();
 });
 
 const btnBackspace = document.querySelector('.backspace');
@@ -93,4 +89,47 @@ btnPlusMinus.addEventListener('click', () => {
         let oppSign = flip.substring(1, flip.length);
         numDisplay.textContent = oppSign;
     };
+});
+
+
+////////////////////////////////////////////////////    
+///////////////// Operator Buttons /////////////////
+////////////////////////////////////////////////////
+
+
+const btnDivide = document.querySelector('.divide');
+btnDivide.addEventListener('click', () => {
+    divideArray.push(+numDisplay.textContent);
+    numDisplay.textContent = '';
+});
+    
+const btnMultiply = document.querySelector('.multiply');
+btnMultiply.addEventListener('click', () => {
+    multArray.push(+numDisplay.textContent);
+    numDisplay.textContent = '';
+});
+
+const btnSubtract = document.querySelector('.subtract');
+btnSubtract.addEventListener('click', () => {
+    subArray.push(+numDisplay.textContent);
+    numDisplay.textContent = '';
+});
+    
+const btnAdd = document.querySelector('.add');
+btnAdd.addEventListener('click', () => {
+    numArr.push(+numDisplay.textContent);
+    if (numArr[1]) {
+        let sum = numArr[0] + numArr[1];
+        numArr = [];
+        numArr.push(sum);
+        numDisplay.textContent = '';
+    } else {
+        numDisplay.textContent = '';
+    }
+});
+
+const btnEquals = document.querySelector('.equals');
+btnEquals.addEventListener('click', () => {
+    if()
+    numDisplay.textContent = numArr[0];
 });
